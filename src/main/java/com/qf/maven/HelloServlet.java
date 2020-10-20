efa4d90c9b2ed2e3b72ee157964ad2964ee79365
Yachiyo3 <73152378+Yachiyo3@users.noreply.github.com>
2020-10-20 19:38:18 +0800

    Update HelloServlet.java

--- src/main/java/com/qf/maven/HelloServlet.java
+++ src/main/java/com/qf/maven/HelloServlet.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 public class HelloServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+            System.out.println("喝了咯");
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
